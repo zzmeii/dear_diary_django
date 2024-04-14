4db8aa2add82a353d23e556913d131353bd6867f
Logvinovich Oleg <logvinovicholeg@gmail.com>
2024-04-14 23:25:45 +0700

    views cleanup

--- diary_app/views.py
+++ diary_app/views.py
@@ -1,40 +1,9 @@
-from django.http import JsonResponse
-from django.shortcuts import render
-from rest_auth.views import LoginView
-
-from rest_framework.generics import ListAPIView, DestroyAPIView, CreateAPIView
-from rest_framework.permissions import IsAdminUser
-from rest_framework.status import HTTP_404_NOT_FOUND
-from rest_framework.views import APIView
-from rest_framework.viewsets import ViewSet, ModelViewSet
-
-from .models import Diary
-from .serializers import *
 
 
+from rest_framework.viewsets import  ModelViewSet
 
-# class CustomLoginView(LoginView):
-#
-
-
-class CustomGetAPIView(APIView):
-    model = None
-    serializer_class = None
-
-    def get(self, request, pk):
-        obj = self.model.objects.filter(id=pk).first()
-        if obj:
-            return JsonResponse(self.serializer_class(instance=obj).data)
-        else:
-            return JsonResponse({'error': 'Object not found'}, status=HTTP_404_NOT_FOUND)
-
-
-class CustomDeleteAPIView(DestroyAPIView):
-    model = None
-
-    def get_object(self):
-        return self.model.objects.filter(id=self.kwargs.get('pk')).first()
-
+from .models import Diary, Note
+from .serializers import DiarySerializer, NoteSerializer
 
 class DiaryModelViewSet(ModelViewSet):
     serializer_class = DiarySerializer
