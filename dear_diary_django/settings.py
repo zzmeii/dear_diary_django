ca5d7ae5075c33053f708304a388143833808b90
Logvinovich Oleg <logvinovicholeg@gmail.com>
2024-04-14 20:16:31 +0700

    docker done

--- dear_diary_django/settings.py
+++ dear_diary_django/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,11 +75,11 @@ WSGI_APPLICATION = "dear_diary_django.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "diary_db",
-        "USER": "yona",
-        "PASSWORD": "321123",
-        "HOST": "localhost",
-        "PORT": "",
+        "NAME": os.getenv("POSTGRES_DB", "diary_db"),
+        "USER": os.getenv("POSTGRES_USER", "yona"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "321123"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
 
